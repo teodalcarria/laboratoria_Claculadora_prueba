4d6dcac8570a264175ab7e078addefd6eca99e88
Tomas Escudero <teodalcarria@gmail.com>
2024-04-25 00:51:58 +0200

    dev1 añade funcionalidad a CalculadoraService

--- src/servicio/CalculadoraDaoImpl.java
+++ src/servicio/CalculadoraDaoImpl.java
@@ -1,5 +1,35 @@
 package servicio;
 
-public class CalculadoraDaoImpl {
+public class CalculadoraDaoImpl implements CalculadoraDao{
+
+	@Override
+	public int sumar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return op1 + op2;
+	}
+
+	@Override
+	public int restar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return op1 - op2;
+	}
+
+	@Override
+	public int multiplicar(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return op1 * op2;
+	}
+
+	@Override
+	public int dividir(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return op1 / op2;
+	}
+
+	@Override
+	public int resto(int op1, int op2) {
+		// TODO Auto-generated method stub
+		return op1 % op2;
+	}
 
 }
